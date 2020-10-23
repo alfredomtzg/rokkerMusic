46ec497cec0296b28ddc6e2292e7dd037e5285f0
shukonin <fernando.canas@outlook.com>
2020-10-23 16:02:12 -0500

    fix/ Last commit did not include index.js for SignUp Demographics page

---
+++ src/pages/SignUpDemographics/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import MainContainer from '../../containers/MainContainer'
+import { TopBar, TopTextBox, NextButtonBox, FormBox } from '../Globals/GlobalStyle'
+
+const SignUpEntryData = () => {
+  return(
+    <MainContainer dark>
+      <TopBar/>
+      <TopTextBox>
+        <h2>And Mike,</h2>
+        <h5>So we can serve you better</h5>
+      </TopTextBox>
+      <FormBox>
+
+      </FormBox>
+      <NextButtonBox>
+        
+      </NextButtonBox>
+    </MainContainer>
+  )
+}
+
+export default SignUpEntryData;
