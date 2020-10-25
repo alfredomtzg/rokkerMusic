b702e5d7d32c142c91f3f1a472ab459192c9e1a3
Mario Silva <mario.silvae@outlook.com>
2020-10-25 12:36:26 -0600

    [Feat]: Cards component for playlist pages

---
+++ src/components/Cards/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "../../assets/fonts/coolicons.css";
+import { StyledMidCard, StyledSmallCard, StyledWideCard } from "./styles";
+import Logo from "../../assets/images/logo-rokker.png";
+
+export const WideCard = () => {
+  return (
+    <StyledWideCard>
+      <i className="ci-heart_fill"></i>
+      <i className="ci-play_circle_filled black"></i>
+    </StyledWideCard>
+  );
+};
+
+export const MidFilledCard = () => {
+  return (
+    <StyledMidCard>
+      <img src={Logo} alt="" />
+      <p>Your playlist name</p>
+      <i className="ci-play_circle_filled"></i>
+    </StyledMidCard>
+  );
+};
+
+export const MidFavoriteCard = () => {
+  return (
+    <StyledMidCard>
+      <i className="ci-heart_fill"></i>
+      <i className="ci-play_circle_filled black"></i>
+    </StyledMidCard>
+  );
+};
+
+export const SmallFilledCard = () => {
+  return (
+    <StyledSmallCard>
+      <img src={Logo} alt="" />
+      <p>Your playlist name</p>
+      <i className="ci-play_circle_filled"></i>
+    </StyledSmallCard>
+  );
+};
+
+export const SmallFavoriteCard = () => {
+  return (
+    <StyledSmallCard>
+      <i className="ci-heart_fill"></i>
+      <i className="ci-play_circle_filled black"></i>
+    </StyledSmallCard>
+  );
+};
+
+export const SmallPlusCard = () => {
+  return (
+    <StyledSmallCard>
+      <i className="ci-plus"></i>
+    </StyledSmallCard>
+  );
+};
---
+++ src/components/Cards/styles.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import { Colors } from "../styles/globals";
+
+export const StyledMidCard = styled.div`
+  position: relative;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 13.8rem;
+  height: 13.8rem;
+  border-radius: 1rem;
+  background-color: ${Colors.G200};
+  box-shadow: ${Colors.shadow};
+
+  img {
+    width: 13.8rem;
+    height: 13.8rem;
+    object-fit: contain;
+    border-radius: 0.5rem;
+  }
+
+  p {
+    position: absolute;
+    margin: 0;
+    color: ${Colors.G100};
+  }
+
+  .ci-play_circle_filled {
+    position: absolute;
+    left: 1rem;
+    bottom: 1rem;
+    font-size: 2rem;
+    color: ${Colors.G100};
+  }
+
+  .ci-heart_fill {
+    font-size: 5.2rem;
+    color: ${Colors.R500};
+  }
+
+  .black {
+    color: ${Colors.S700};
+  }
+`;
+
+export const StyledSmallCard = styled(StyledMidCard)`
+  width: 8rem;
+  height: 8rem;
+
+  img {
+    width: 8rem;
+    height: 8rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+
+  .ci-play_circle_filled {
+    font-size: 1.5rem;
+    left: 0.5rem;
+    bottom: 0.5rem;
+  }
+
+  .ci-heart_fill {
+    font-size: 3.5rem;
+  }
+
+  .ci-plus {
+    font-size: 3rem;
+  }
+`;
+
+export const StyledWideCard = styled(StyledMidCard)`
+  width: 33.5rem;
+`;
