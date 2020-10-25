5f4547d93ba62f265c8bdfc8124e2f39bf10e69e
shukonin <fernando.canas@outlook.com>
2020-10-24 23:48:35 -0500

    feature/ fullscreen Player mockup added

---
+++ src/components/FullscreenPlayer/style.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components'
+import {Colors} from '../styles/globals'
+
+export const TopOptions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2vh 8vw;
+  align-items: center;
+  color: ${Colors.G700};
+
+  div {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  i {
+    font-size: 2.4rem;
+  }
+`
+
+export const SongCover = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 7vh 0;
+`
+
+export const SongTitleAndArtist = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 6vh 0;
+
+  h3 {
+    color: ${Colors.G700};
+  }
+
+  h4 {
+    color: ${Colors.G600};
+  }
+`
+
+export const Timeline = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 7vh 9vw 0;
+  color: ${Colors.G600};
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+`
+
+export const TotalBar = styled.div`
+  position: relative;
+  height: 1vh;
+  width: 50vw;
+  background-color: ${Colors.G400}  
+`
+
+export const ProgressBar = styled.div`
+  position: absolute;
+  height: 1vh;
+  width: 40vw;
+  background-color: ${Colors.R500};
+`
+
+export const PlayerMedia = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${Colors.G200};
+`
+
+export const PlayerButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 2.4rem;
+  margin: 2vh 6vw;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  .ci-play_circle_filled {
+    font-size: 5rem;
+    margin: 0 2vw;
+  }
+
+  .ci-heart_fill {
+    margin-left: 3.4rem;
+  }
+
+  .ci-shuffle {
+    margin-right: 1rem;
+  }
+`
+
+export const PlayerBottomButtons = styled(PlayerButtons)``
