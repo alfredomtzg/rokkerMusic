223b5202984ff45ff922b5b822d7e2240c86a788
Mario Silva <mario.silvae@outlook.com>
2020-10-24 20:23:27 -0500

    [Update]: change pixel to rem

--- src/components/minimizedPlayer/styles.js
+++ src/components/minimizedPlayer/styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
-
+import { Colors } from "../styles/globals";
 export const StyledMinimizedPlayer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  height: 50px;
-  padding: 0px 20px;
-  color: white;
-  background: rgba(62, 42, 209, 1);
+  height: 5rem;
+  padding: 0 2rem;
+  color: ${Colors.white};
+  background: ${Colors.B500};
 `;
 
 export const StyledIconsLeft = styled.div`
@@ -15,20 +15,20 @@ export const StyledIconsLeft = styled.div`
   align-items: center;
 
   i {
-    font-size: 24px;
-    padding-right: 20px;
+    font-size: 2.4rem;
+    padding-right: 2rem;
   }
 
   h6,
   p {
     margin: 0;
-    padding-bottom: 5px;
+    padding-bottom: 0.5rem;
   }
 `;
 
 export const StyledIconsRight = styled.div`
   i {
-    padding: 10px;
-    font-size: 24px;
+    padding: 1rem;
+    font-size: 2.4rem;
   }
 `;
