4d69a30f41afc97daaa65c7deeda3370708550cb
shukonin <fernando.canas@outlook.com>
2020-10-25 22:01:47 -0500

    feature/ Footer MockUp added

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./style";
+import "../../assets/fonts/coolicons.css";
+
+const Footer = () => {
+  return (
+    <Navbar>
+      <i className="ci-home_alt_fill" />
+      <i className="ci-grid_round" />
+      <i className="ci-search" />
+    </Navbar>
+  );
+};
+
+export default Footer;
