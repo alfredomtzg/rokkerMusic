d0a70d90b6d4e916f8e83f84f6484370ab89f707
shukonin <fernando.canas@outlook.com>
2020-10-27 10:29:15 -0500

    fix/ Settings User Card added and scroll adjusted

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { UserCardContainer, ImgContainer } from "./style";
-import { TextLabel, TextInput } from "../Forms/dark-styles";
+import { TextLabel, TextInput, FormContainer } from "../Forms/light-styles";
 import Avatar from "../../assets/images/avatars/Alexander.svg";
 
 export default function UserCard() {
   // Values Profile
   const [valuesProfile, setValuesProfile] = useState({
-    name: "Alfredo test",
-    email: "Correo@test",
+    name: "Mike Riise",
+    email: "mike@rokker.com",
   });
 
   return (
@@ -15,20 +15,22 @@ export default function UserCard() {
       <ImgContainer>
         <img src={Avatar} alt="avatar" />
       </ImgContainer>
-      <TextLabel htmlFor="name">
-        <h4>Name</h4>
-      </TextLabel>
-      <TextInput id="name" disabled name="name" value={valuesProfile.name} />
-      <TextLabel htmlFor="email">
-        <h4>E-mail</h4>
-      </TextLabel>
-      <TextInput
-        id="email"
-        disabled
-        name="email"
-        autoComplete="current-email"
-        value={valuesProfile.email}
-      />
+      <FormContainer>
+        <TextLabel htmlFor="name">
+          <h4>Name</h4>
+        </TextLabel>
+        <TextInput id="name" disabled name="name" value={valuesProfile.name} />
+        <TextLabel htmlFor="email">
+          <h4>E-mail</h4>
+        </TextLabel>
+        <TextInput
+          id="email"
+          disabled
+          name="email"
+          autoComplete="current-email"
+          value={valuesProfile.email}
+        />
+      </FormContainer>
     </UserCardContainer>
   );
 }
