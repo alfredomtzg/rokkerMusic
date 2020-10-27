d8585cdc9e8389b8678a95baef4d4e61170e6250
shukonin <fernando.canas@outlook.com>
2020-10-27 10:38:29 -0500

    fix/ Settings Change Password Page containers adjusted for proper scroll-down

---
+++ src/pages/SettingsChangePassword/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const UserAvatarBox = styled.figure`
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 10rem;
+  }
+`;
+
+export default UserAvatarBox;
