390c1bde8bf60bb417190850533cbf3bc84a73a0
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-27 14:02:03 -0500

    fix/file App.js

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
-<<<<<<< HEAD
-=======
-import SearchResults from "../pages/SearchResults";
->>>>>>> 4092226797f6645ebb4168aca2ba02ceab439515
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-<<<<<<< HEAD
-=======
-      <SearchResults />
->>>>>>> 4092226797f6645ebb4168aca2ba02ceab439515
     </div>
   );
 }
