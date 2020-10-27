d2d6b074aa9f16ba7388280c7ac149c6573f47e0
shukonin <fernando.canas@outlook.com>
2020-10-26 22:29:47 -0500

    feature/ Inserted components in Page 404

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
-import FullscreenPlayerPage from "../pages/FullscreenPlayer";
+import Landing from "../pages/Landing";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <FullscreenPlayerPage />
+      <Landing />
     </div>
   );
 }
