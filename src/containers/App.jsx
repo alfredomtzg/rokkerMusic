f6f215511d6e5862e3736ccb53cf431057b004cb
shukonin <fernando.canas@outlook.com>
2020-10-27 21:01:13 -0500

    fix/ Inserted Artist Profile Card component which was missing

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
-import PlaylistContent from "../pages/PlaylistContent";
+import ArtistProfile from "../pages/ArtistProfile";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <PlaylistContent />
+      <ArtistProfile />
     </div>
   );
 }
