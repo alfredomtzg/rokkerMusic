ee20a86838f8678a1dd5c5789367aec1303e45f7
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-28 12:48:01 -0500

    delete/own files with conflict

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
-<<<<<<< HEAD
-import {ModalDottedMenu} from '../components/Modals/ModalDottedMenú/index'
-=======
-import Landing from "../pages/Landing";
->>>>>>> 3ed0f1982e0ea5da6a357093b0fec1d62eef89e4
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-<<<<<<< HEAD
-      <ModalDottedMenu/>
-=======
-      <Landing />
->>>>>>> 3ed0f1982e0ea5da6a357093b0fec1d62eef89e4
     </div>
   );
 }
