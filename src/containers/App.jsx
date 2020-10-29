b25afcf41e971458192ef1c239530ee48361ed73
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-29 17:00:34 -0500

    update/file App.js

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
-import Loading from "../components/Loading/index";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Loading />
     </div>
   );
 }
