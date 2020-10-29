f9b505caff091683a71080bdf286493bae450eb9
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-29 10:42:25 -0500

    buttons created with label a for the modal

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
-import ButtonAddToQueue from '../components/Modals/AddToQueue/index'
-import ButtonSeeArtistProfile from '../components/Modals/SeeArtistProfile/index'
-import ButtonAddToPlaylist from '../components/Modals/AddToPlaylist/index'
-import ButtonAddRemove from '../components/Modals/Add-Remove/index'
-
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <ButtonAddToPlaylist/>
-      <ButtonAddToQueue/>
-      <ButtonSeeArtistProfile/>
-      <ButtonAddRemove/>
     </div>
   );
 }
