b24d7bb71ae8895ff4ab37d640533568c916d1da
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-29 10:24:22 -0500

    feature/generated addToPlaylist button labeled a

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
 import ButtonAddTQ from '../components/Modals/AddToQueue/index'
 import ButtonSeeAP from '../components/Modals/SeeArtistProfile/index'
+import ButtonAddToPlaylist from '../components/Modals/AddToPlaylist/index'
 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <ButtonSeeAP/>
+      <ButtonAddToPlaylist/>
+    
     </div>
   );
 }
