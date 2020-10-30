201382e45d77c971002a3cce77edb1b6b59d703c
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-30 14:07:46 -0500

    feature/new modal implemented in good condition, I continue to implement the union of two modals

--- src/components/Modals/borrar/style.js
+++ src/components/Modals/borrar/style.js
@@ -20,30 +20,23 @@ export const StyledModalDialog = styled.div`
 `;
 
 export const StyledModalDialog2 = styled.div`
-  width: 40rem;
   position: relative;
-  margin: 10% auto;
-  padding: 0.5rem 2rem 1.3rem 2rem;
-  border-radius: 1rem;
-  background: #fff;
+  margin: 25rem auto;
+/*   padding: 0.5rem 2rem 1.3rem 2rem;
+ */  border-radius: 1rem;
 `;
 export const StyledClose = styled.a`
   background: #606061;
   color: #ffffff;
   line-height: 2.5rem;
   position: absolute;
-  right: 1.2rem;
-  top: 1rem;
+  right: 3.5rem;
+  top: 2rem;
   text-align: center;
   width: 2.4rem;
   text-decoration: none;
-  font-weight: bold;
-  -webkit-border-radius: 1.2rem;
-  -moz-border-radius: 1.2rem; 
   border-radius: 1.2rem;
-   -moz-box-shadow: 0.1rem 0.1rem 0.3rem #000;
-  -webkit-box-shadow: 0.1rem 0.1rem 0.3rem #000; 
-  box-shadow: 0.1rem 0.1rem 0.3rem #000;
+ 
   &:hover {
     background: #00d9ff;
   }
