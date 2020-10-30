7b463f94e2c42d400fedb36f876a5d1b328aed0f
shukonin <fernando.canas@outlook.com>
2020-10-29 23:45:05 -0500

    fix/ app.js returned to Landing

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
 import Routes from "../routes";
-import Modal from "../components/Modal";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Modal />
+      <Routes />
     </div>
   );
 }
