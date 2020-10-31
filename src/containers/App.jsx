bcf1a949ccea85d237346299476d0aa7fb95bedf
Fernando Cañas <fernando.canas@outlook.com>
2020-10-31 08:06:45 -0500

    fix/ routes components was missing

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
+import Routes from "../routes";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Routes />
     </div>
   );
 }
