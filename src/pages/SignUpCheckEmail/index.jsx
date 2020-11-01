8202d0e17bd05df503c90f068b0820d6f8b7e5cd
shukonin <fernando.canas@outlook.com>
2020-11-01 00:28:39 -0500

    feature/ link to login from sign up check email

--- src/pages/SignUpCheckEmail/index.jsx
+++ src/pages/SignUpCheckEmail/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { LoginButton } from "../../components/Buttons";
 import { PageContainer } from "../../containers/LayoutContainers";
 import {
@@ -13,7 +14,9 @@ const SignUpCheckEmail = () => {
         <h1>Rokk It!!!</h1>
         <h3>Please check your email</h3>
         <LoginOrSendButtonBox>
-          <LoginButton />
+          <Link to="/login">
+            <LoginButton />
+          </Link>
         </LoginOrSendButtonBox>
       </FullPageCenterMsgBox>
     </PageContainer>
