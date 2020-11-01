355aa8ac94025edd806d64862789ff1ccd1952cd
Alfredo Martinez <psialfredomtz@gmail.com>
2020-11-01 17:18:59 -0600

    [update] activate finish

--- src/pages/Activation/index.jsx
+++ src/pages/Activation/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
 import {
   PageContainer,
   MainContainer,
@@ -11,24 +11,58 @@ import {
 } from "../Globals/GlobalStyle";
 import Header from "../../components/Header";
 import { LoginButton } from "../../components/Buttons";
+import { API, auth } from "../../route/axios";
 
 const Activation = () => {
+  const { id } = useParams();
+  const history = useHistory();
+
+  console.log(id);
+  console.log(`history : ${history}`);
+
+  const [active, setActive] = useState(false);
+
+  const autenticacion = async () => {
+    const response = await API.get(`${auth}?id=${id}`);
+    console.log(response);
+    if (response.status === 201) {
+      setActive(true);
+    }
+  };
+
+  useEffect(() => {
+    autenticacion();
+  }, []);
+
   return (
     <PageContainer dark>
       <TopBar>
         <Header />
       </TopBar>
-      <MainContainer>
-        <TopTextBox>
-          <h2>Welcome aboard!</h2>
-          <h5>Your account is now active</h5>
-        </TopTextBox>
-        <LoginOrSendButtonBox>
-          <Link to="/login">
-            <LoginButton />
-          </Link>
-        </LoginOrSendButtonBox>
-      </MainContainer>
+      {active ? (
+        <MainContainer>
+          <TopTextBox>
+            <h2>Welcome aboard!</h2>
+            <h5>Your account is now active</h5>
+          </TopTextBox>
+          <LoginOrSendButtonBox>
+            <Link to="/login">
+              <LoginButton />
+            </Link>
+          </LoginOrSendButtonBox>
+        </MainContainer>
+      ) : (
+        <MainContainer>
+          <TopTextBox>
+            <h2>Somentingh went wrong</h2>
+          </TopTextBox>
+          <LoginOrSendButtonBox>
+            <Link to="/">
+              <button type="button">Back</button>
+            </Link>
+          </LoginOrSendButtonBox>
+        </MainContainer>
+      )}
     </PageContainer>
   );
 };
--- src/route/axios.js
+++ src/route/axios.js
@@ -7,6 +7,9 @@ export const API = axios.create({
 // TOKEN
 export const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZjllZjIyODE2MWIxOTAwMTczYTlmYTQiLCJuYW1lIjoiQWxmcmVkbyIsImVtYWlsIjoicHNpYWxmcmVkb210ekBnbWFpbC5jb20iLCJzY29wZXMiOlsic2lnbl9pbjphdXRoIiwic2lnbl91cDphdXRoIiwic2lnbl9wcm92aWRlcjphdXRoIiwicmVhZDphbGJ1bSIsInJlYWQ6YXJ0aXN0IiwicmVhZDpnZW5kZXIiLCJyZWFkOnRyYWNrIiwicmVhZDp1c2VyIiwidXBkYXRlOnVzZXIiLCJyZWFkOnNlYXJjaEhpc3RvcnkiLCJjcmVhdGU6cGxheWxpc3RzIiwidXBkYXRlOnBsYXlsaXN0cyIsImRlbGV0ZTpwbGF5bGlzdHMiLCJyZWFkOnBsYXlsaXN0cyJdLCJpYXQiOjE2MDQyNjE2NTgsImV4cCI6MTYwNDI2MzQ1OH0.q87B9X4FrdA2_eGSCtz87UtFUBCgRdDUwLKpsZJKKJE";
+export const ApiKey = {
+  "apiKeyToken": "ae264722356a385b5b0f9e0cb91e26dc5ccfb3ca90af8f853bb76d89ed758edc"
+}
 
 // create end points
 // Post Album
@@ -15,10 +18,11 @@ export const getAlbum = 'album';
 export const getAlbumId = 'album/:albumid'
 // Create a new play list
 export const PlayList = 'playlist'
-
+// autenticaón
+export const auth = '/auth/activate/'
 // get signUp
 export const signUp = '';
 // check Login
-export const login = '';
+export const login = 'auth';
 // get List Top 20
 export const listTop = '';
