83915724e7065396b67123db010dbde29251e098
Sergio Hortua <sergioandresjsv@gmail.com>
2020-11-11 16:27:08 -0500

    Fix/file App.js

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "../components/styles/globals";
+import Routes from "../routes";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Routes />
     </>
   );
 }
